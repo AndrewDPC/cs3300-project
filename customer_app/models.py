61d8f2bc9dc38a156b9e81e49b004fc123929849
Andrew Douangprachanh <andrew.dpc0407@gmail.com>
2023-10-27 18:03:27 -0600

    First commit

---
+++ customer_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+#Class for a Lego set on display for users to look at
+#class LegoSet(models.Model):
+    
+    #Image of the Lego set on display
+    #Name of the Lego set on display
+    #Description of the Lego set on display
+    #Overall rating of the Lego set on display
---
+++ customer_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#Home Page
+def index(request):
+    return render( request, 'customer_app/index.html')
